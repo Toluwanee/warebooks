8a7e848182c5630bbb64e4fec3929300e52efbc7
Toluwanee <toluwalope9@gmail.com>
2023-09-17 23:01:42 +0100

    first push to branch

--- client/src/pages/landingpage/contact/Contact.jsx
+++ client/src/pages/landingpage/contact/Contact.jsx
@@ -1,5 +1,30 @@
 export const Contact = () => {
     return (
-      <div>Contact</div>
+      <div>
+        <h1><strong>Contact Us</strong></h1>
+        <br />
+
+        <p>
+        Thank you for your interest in Utility Ware Ltd and our product Ware Book. We value your feedback, inquiries, and partnership opportunities. 
+        Please do not hesitate to reach out to us using the contact information below:
+
+Email: hello@utilityware.co
+
+Phone: +234 70 1312 3835
+
+Our team is dedicated to providing prompt and helpful assistance. 
+Whether you have questions about our services, require technical support, or wish to explore potential collaborations, we are here to assist you.
+
+Address:
+Plot 1099, Umar Shuaibu Ave, N. Okonjo-Iweala Way
+
+Feel free to drop us a message or give us a call, and we will get back to you as soon as possible. 
+Your input is vital to our mission of revolutionizing digital publishing and empowering authors and readers alike.
+
+Thank you for choosing Utility Ware Ltd. 
+We look forward to hearing from you and working together to make Ware Book a valuable asset to the literary world.
+        </p>
+
+      </div>
     )
   }
